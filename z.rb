2b656f89541048f59b301fca312f9fb79922248b
Stas Ukolov <ukoloff@gmail.com>
2015-02-28 21:55:53 +0500

    Access certificates

--- z.rb
+++ z.rb
@@ -1,4 +1,5 @@
 require 'ffi'
+require 'openssl'
 
 module Crypt
   extend FFI::Library
@@ -9,6 +10,14 @@ module Crypt
   attach_function :close, :CertCloseStore, [:pointer, :uint], :int
   attach_function :enum, :CertEnumCertificatesInStore, [:pointer, :pointer], :pointer
 
+  class Ctx < FFI::Struct
+    layout :dwCertEncodingType, :uint,
+      :pbCertEncoded, :pointer,
+      :cbCertEncoded, :uint,
+      :pCertInfo, :pointer,
+      :hCertStore, :pointer
+  end
+
   def self.run
     store = open(nil, 'ROOT')
     p store
@@ -17,7 +26,9 @@ module Crypt
       while true
         ctx = enum store, ctx
         break if ctx.null?
-        p ctx
+        z = Ctx.new ctx
+        crt = OpenSSL::X509::Certificate.new z[:pbCertEncoded].read_string z[:cbCertEncoded]
+        puts crt.subject
       end
     ensure
       close store, 0
