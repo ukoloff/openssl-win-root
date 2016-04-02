86eb6998018e6c048047fb450e5a156019c4520c
Stas Ukolov <ukoloff@gmail.com>
2016-04-02 21:05:31 +0500

    Clean syntax

--- test/http.rb
+++ test/http.rb
@@ -4,12 +4,10 @@ class TestEngine < Minitest::Test
 
   def test_http
     assert_raises(OpenSSL::SSL::SSLError) do
-      Net::HTTP.get(URI 'https://ya.ru')
+      Net::HTTP.get URI 'https://ya.ru'
     end
     require 'openssl/win/root'
-    Net::HTTP.get(URI 'https://ya.ru')
-
+    Net::HTTP.get URI 'https://ya.ru'
   end
 
 end
-
