c07a2d318b0868e396b4f6720ee086cc942c1956
Stas Ukolov <ukoloff@gmail.com>
2016-04-02 20:26:26 +0500

    HTTPS connect twice

---
+++ test/http.rb
@@ -0,0 +1,15 @@
+require 'net/http'
+
+class TestEngine < Minitest::Test
+
+  def test_http
+    assert_raises(OpenSSL::SSL::SSLError) do
+      Net::HTTP.get(URI 'https://ya.ru')
+    end
+    require 'openssl/win/root'
+    Net::HTTP.get(URI 'https://ya.ru')
+
+  end
+
+end
+
