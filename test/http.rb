abc1a7b9955a6c11a2757beacbbce7b3b41e206c
Stas Ukolov <ukoloff@gmail.com>
2016-04-02 22:18:53 +0500

    Change test site

--- test/http.rb
+++ test/http.rb
@@ -2,12 +2,14 @@ require 'net/http'
 
 class TestEngine < Minitest::Test
 
+  WWW='https://www.appveyor.com/'
+
   def test_http
     assert_raises(OpenSSL::SSL::SSLError) do
-      Net::HTTP.get URI 'https://ya.ru'
+      Net::HTTP.get URI WWW
     end
     require 'openssl/win/root'
-    Net::HTTP.get URI 'https://ya.ru'
+    Net::HTTP.get URI WWW
   end
 
 end
